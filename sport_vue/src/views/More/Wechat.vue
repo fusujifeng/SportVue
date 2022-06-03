674491692c24a6ce50b614e34fa7c59973970a92
fusujifeng <71201851+fusujifeng@users.noreply.github.com>
2022-06-03 16:43:16 +0800

    实现前端聊天室

--- sport_vue/src/views/More/Wechat.vue
+++ sport_vue/src/views/More/Wechat.vue
@@ -3,6 +3,17 @@
   <div class="message">
     <el-card>
   <h3>请联系我</h3>
+      <div class="content">
+        <div v-for="e in arr" >{{e}}</div>
+        <div class="main">
+          <div class="foot">
+            <el-input class="cont" v-model="cont" placeholder="请输入内容"></el-input>
+            <el-button type="success" @click="sendMessage">发送</el-button>
+
+          </div>
+
+        </div>
+      </div>
     </el-card>
   </div>
 
@@ -11,11 +22,22 @@
 
 <script>
 export default {
-  name: "Wechat"
+  name: "Wechat",
+  data(){
+      return{
+        cont:'',
+        arr:['ddd','segg','qerrr'],
+      }
+    },
+  methods:{
+    sendMessage(){
+      this.arr.push(this.cont)
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .message {
   width: 100%;
   height: 100%;
@@ -27,5 +49,26 @@ export default {
   width: 800px;
   height: 600px;
   border-radius: 15px;
+}
+.content{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+}
+
+.title{
+  font-size: 36px;
+  color:#8f8f94;
+}
+.foot{
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  .cont{
+    width: 100%;
+    height: 80px;
+    background-color: #eee;
+  }
 }
 </style>
